906a2f9b79b15f9cde0b746a1b1e5307e9e700d1
Francisco Joshua Quintero Montero <joshua.qm@comunidad.unam.mx>
2025-06-20 08:56:40 -0600

    [UPDATE] Protected routes
    El usuario sin permiso ya no puede acceder a las rutas a las que no tiene permiso entrar por rol

--- routes/admin.php
+++ routes/admin.php
@@ -6,5 +6,7 @@ use App\Http\Controllers\Admin\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('categories', CategoryController::class);
-Route::resource('posts', PostController::class);
+Route::middleware(['auth', 'can:crear post'])->group(function () {
+    Route::resource('posts', PostController::class);
+});
 Route::resource('tags', TagController::class);
