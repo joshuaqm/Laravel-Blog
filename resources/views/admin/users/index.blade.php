c98941d0f7ae49ff9887eaa546349cd24cb0ac26
Francisco Joshua Quintero Montero <joshua.qm@comunidad.unam.mx>
2025-06-20 09:18:08 -0600

    [CREATE] View users
    Creé la vista para ver los usuarios registrados

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,67 @@
+<x-layouts.app>
+    <div class="mb-8 flex justify-between items-center">
+        <flux:breadcrumbs>
+            <flux:breadcrumbs.item href="{{ route('dashboard') }}" icon="home" />
+            <flux:breadcrumbs.item href="{{ route('admin.users.index') }}">Users</flux:breadcrumbs.item>
+        </flux:breadcrumbs>
+
+        <a href="{{ route('admin.users.create') }}" class="btn btn-blue">Add User</a>
+    </div>
+
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        ID
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nombre
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Correo
+                    </th>
+                    <th scope="col" class="px-6 py-3" width="10px">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        <th scope="row"
+                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $user->id }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $user->name }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $user->email }}
+                        </td>
+                        <td class="px-6 py-4">
+                            <div class="flex space-x-2">
+                                <a href="{{ route('admin.users.edit', $user) }}" class="btn btn-blue text-xs">
+                                    Editar
+                                </a>
+                                <form class="delete-form" action="{{ route('admin.users.destroy', $user) }}"
+                                    method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-red text-xs">
+                                        Eliminar
+                                    </button>
+                                </form>
+                            </div>
+
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    {{-- <div class="mt-4">
+        {{ $user->links() }}
+    </div> --}}
+
+</x-layouts.app>
