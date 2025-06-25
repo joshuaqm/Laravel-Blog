13c5d3afe32123b64eb79fad19f1277129372bd4
Francisco Joshua Quintero Montero <joshua.qm@comunidad.unam.mx>
2025-06-25 16:52:22 -0600

    Add ComputedComponent Livewire component
    Introduces a new Livewire component, ComputedComponent, with a computed property for fetching a Post by ID. Updates the dashboard view to include this component and adds its corresponding Blade view.

---
+++ resources/views/livewire/computed-component.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <div class="mb-4">
+        <x-input class="w-full" type="number" wire:model.live="post_id" />
+    </div>
+    <div class="mb-4">
+        {{ $this->post }}
+    </div>
+</div>
